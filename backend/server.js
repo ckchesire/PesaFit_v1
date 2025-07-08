d8381a461c6779889be21ff41ff83ec981ed529e
Christian Chesire <christianck.chesire@gmail.com>
2025-07-08 17:44:54 +0300

    feat: added authentication API routes for registration, login, and user info

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -15,10 +16,13 @@ app.use(
   })
 );
 
+// Middleware for pesing JSON requests
 app.use(express.json());
 
 
 connectDB();
+
+app.use("/api/v1/auth",authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
