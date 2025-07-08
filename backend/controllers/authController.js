d621cf38eaac2bd4830b191787a1e7196091b494
Christian Chesire <christianck.chesire@gmail.com>
2025-07-08 17:51:22 +0300

    feat: implement authController with register, login, and retrieve user details logic

---
+++ backend/controllers/authController.js
@@ -0,0 +1,51 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+// Function to generate JWT token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h"});
+}
+
+// Register User
+exports.registerUser = async (req, res) => {
+  console.log("Register User Request:", req.body);
+  // Defensive destructuring to handle cases where req.body might be undefined
+  const { fullName, email, password, profileImageUrl } = req.body || {};
+
+  // Perform validation for missing fields
+  if (!fullName || !email || !password) {
+    return res.status(400).json({ message: "All fields are required"})
+  }
+
+  try {
+    // Check if user email already exists
+    const existingUser = await User.findOne({ email});
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    // Create a new user
+    const user = await User.create({
+      fullName,
+      email,
+      password,
+      profileImageUrl,
+    });
+
+    res.status(201).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: err.message });
+  }
+};
+
+// Login User
+exports.loginUser = async (req, res) => {};
+
+// Get User Info
+exports.getUserInfo = async (req, res) => {};
