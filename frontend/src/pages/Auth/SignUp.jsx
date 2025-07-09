a1b58c9aa5ee95349fd4ee87b29ea4e4f12d4caf
Christian Chesire <christianck.chesire@gmail.com>
2025-07-09 19:24:32 +0300

    feat: add image upload helper and integrate SignUp API with token storage and navigation

--- frontend/src/pages/Auth/SignUp.jsx
+++ frontend/src/pages/Auth/SignUp.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import AuthLayout from '../../components/layouts/AuthLayout'
-import Input from '../../components/inputs/Input'
-import { Link } from 'react-router-dom'
-import { validateEmail } from '../../utils/helper'
-import ProfilePhotoSelector from '../../components/inputs/ProfilePhotoSelector'
+import React from 'react';
+import { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AuthLayout from '../../components/layouts/AuthLayout';
+import Input from '../../components/inputs/Input';
+import { Link } from 'react-router-dom';
+import { validateEmail } from '../../utils/helper';
+import ProfilePhotoSelector from '../../components/inputs/ProfilePhotoSelector';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { UserContext } from '../../context/UserContext';
+import uploadImage from '../../utils/uploadImage';
 
 const SignUp = () => {
   const [profilePic, setProfilePic] = useState(null);
@@ -16,6 +20,7 @@ const SignUp = () => {
 
   const [error, setError]  = useState(null);
 
+  const {updateUser} = useContext(UserContext);
   const navigate = useNavigate();
 
   // Function to handle Sign Up Form Submit
@@ -42,7 +47,36 @@ const SignUp = () => {
     setError("");
 
     // Add Sign Up API call here
-  }
+    try {
+
+      // Upload image if present
+      if (profilePic) {
+        const imgUploadRes = await uploadImage(profilePic);
+        profileImageUrl = imgUploadRes.imageUrl || "";
+      }
+
+      const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, {
+        fullName,
+        email,
+        password,
+        profileImageUrl,
+      });
+
+      const { token, user } = response.data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        updateUser(user);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("Something went wrong. Please try again.");
+      }
+    }
+  };
 
   return (
     <AuthLayout>
